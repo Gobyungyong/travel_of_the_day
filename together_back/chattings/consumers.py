8db3ecb09e3cc41a050301ef906153af98a795e1
byungyong <quddyddlrk@naver.com>
2023-09-06 01:59:02 +0900

    Feat:웹소켓 사용자 인증 로직 추가

--- together_back/chattings/consumers.py
+++ together_back/chattings/consumers.py
@@ -1,28 +1,42 @@
-from channels.generic.websocket import JsonWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from rest_framework_simplejwt.tokens import TokenError, AccessToken
+from urllib.parse import parse_qs
 from asgiref.sync import async_to_sync
 
 
-class ChattingConsumer(JsonWebsocketConsumer):
+class ChattingConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.room_name = None
 
-    def connect(self):
-        print("Connected!")
-        self.room_name = "home"
-        self.accept()
+    async def connect(self):
+        query_string = self.scope["query_string"].decode()
+        token_param = parse_qs(query_string).get("token", [""])[0]
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_name,
-            self.channel_name,
-        )
-        # welcome message 송신
-        self.send_json(
-            {
-                "type": "welcome_message",
-                "message": "Hey there! You've successfully connected!",
-            }
-        )
+        try:
+            access_token = AccessToken(token_param)
+            user = access_token.user
+        except TokenError:
+            user = None
+
+        if user is None:
+            await self.close()
+        else:
+            print("Connected!")
+            self.room_name = "home"
+            self.accept()
+
+            async_to_sync(self.channel_layer.group_add)(
+                self.room_name,
+                self.channel_name,
+            )
+            # welcome message 송신
+            self.send_json(
+                {
+                    "type": "welcome_message",
+                    "message": "Hey there! You've successfully connected!",
+                }
+            )
 
     def disconnect(self, code):
         print("Disconnected!")
