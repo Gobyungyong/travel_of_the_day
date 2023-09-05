3b12c9fd5bb7d1ff1136ada40ead2d080b15d96b
byungyong <quddyddlrk@naver.com>
2023-09-05 14:42:48 +0900

    Feat:channels 위한 초기 consumer 작성

--- together_back/chattings/consumers.py
+++ together_back/chattings/consumers.py
@@ -1,49 +1,32 @@
-import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 
 
-class ChattingConsumer(WebsocketConsumer):
-    # websocket 연결 시
+class ChattingConsumer(JsonWebsocketConsumer):
+    """
+    This consumer is used to show user's online status,
+    and send notifications.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_name = None
+
     def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-
-        # room group
-        self.room_group_name = "chat_%s" % self.room_name
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-
+        print("Connected!")
+        self.room_name = "home"
         self.accept()
-
-    # websocket 연결 종료 시
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
+        # welcome message 송신
+        self.send_json(
+            {
+                "type": "welcome_message",
+                "message": "Hey there! You've successfully connected!",
+            }
         )
 
-    # websocket 메세지 수신 시
-    # def receive(self, text_data):
-    #     text_data_json = json.loads(text_data)
-    #     message = text_data_json['message']
-    #     self.send(text_data=json.dumps({
-    #         'message': message
-    #     }))
+    def disconnect(self, code):
+        print("Disconnected!")
+        return super().disconnect(code)
 
-    # room group
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        text = text_data_json["text"]
-        sender = text_data_json["sender"]
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {"type": "chat_message", "message": text, "sender": sender},
-        )
-
-    def chat_message(self, event):
-        # Receive message from room group
-        text = event["message"]
-        sender = event["sender"]
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({"text": text, "sender": sender}))
+    def receive_json(self, content, **kwargs):
+        print(content)
+        return super().receive_json(content, **kwargs)
