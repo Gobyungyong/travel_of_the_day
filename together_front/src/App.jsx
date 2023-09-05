4c65a392cf3a2789669fe8e88a345d607fad1792
byungyong <quddyddlrk@naver.com>
2023-09-05 21:12:54 +0900

    Feat:라우터 추가

--- together_front/src/App.jsx
+++ together_front/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Chattings from "./pages/Chattings";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import routes from "./routes";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={routes.index} element={<Navbar />}>
+          <Route path="" element={<Chattings />} />
+          <Route path={routes.login} element={<Login />} />
+          <Route path={routes.signup} element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
