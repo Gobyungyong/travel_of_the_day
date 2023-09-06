35ad44ac37a35296bebb7c08644d351ca91c7ed5
byungyong <quddyddlrk@naver.com>
2023-09-06 10:41:04 +0900

    Feat:대댓글 모델 작성

--- together_back/recomments/models.py
+++ together_back/recomments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Recomment(models.Model):
+    content = models.CharField(max_length=150)
+    writer = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="recomment"
+    )
+    comment = models.ForeignKey(
+        "comments.Comment", on_delete=models.CASCADE, related_name="recomment"
+    )
