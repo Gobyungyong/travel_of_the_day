5fa043f539a423c0de5094f2a8136728d1bdb1e5
byungyong <quddyddlrk@naver.com>
2023-09-06 10:34:43 +0900

    Feat:게시글 모델 작성

--- together_back/boards/models.py
+++ together_back/boards/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    subject = models.CharField(max_length=30)
+    content = models.TextField()
+    writer = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="board"
+    )
