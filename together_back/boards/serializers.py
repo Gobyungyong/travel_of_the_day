9009f5b94bbff263769beda2f9026bbd870e5eb9
byungyong <quddyddlrk@naver.com>
2023-09-06 12:53:01 +0900

    Fix:BoardSerializer 작성자 serializer 적용

--- together_back/boards/serializers.py
+++ together_back/boards/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 
 from .models import Board
+from users.serializers import UserInfoSerializer
 
 
 class BoardSerializer(ModelSerializer):
+    writer = UserInfoSerializer(read_only=True)
+
     class Meta:
         model = Board
         fields = "__all__"
