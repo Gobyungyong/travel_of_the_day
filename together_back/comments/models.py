fc2cb91b54107283ee700510d57d3f3c7d5e2059
byungyong <quddyddlrk@naver.com>
2023-09-06 10:36:30 +0900

    Feat:댓글 모델 작성

--- together_back/comments/models.py
+++ together_back/comments/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    content = models.CharField(max_length=150)
+    writer = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="comment"
+    )
