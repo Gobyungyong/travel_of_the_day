0e55f994d9af0e87aa56136ab5c65ae2da124a6a
byungyong <quddyddlrk@naver.com>
2023-09-06 13:19:39 +0900

    Feat:댓글 작성 API 및 url 작성

--- together_back/comments/urls.py
+++ together_back/comments/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 # api/v1/comments/
-# urlpatterns = [
-#     path("", views.AllBoards.as_view()),
-# ]
+urlpatterns = [
+    path("", views.NewComment.as_view()),
+]
--- together_back/comments/views.py
+++ together_back/comments/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import ParseError, NotFound, PermissionDenied
 
-# Create your views here.
+from .models import Comment
+from .serializers import CommentSerializer
+
+
+class NewComment(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            new_comment = serializer.save(writer=request.user)
+            return Response(
+                CommentSerializer(new_comment).data, status=status.HTTP_201_CREATED
+            )
+
+        raise ParseError("Data validation 실패")
