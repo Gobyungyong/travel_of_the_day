a84cca54912d854e85ece95dd7d328a6486bce44
byungyong <quddyddlrk@naver.com>
2023-09-07 20:12:24 +0900

    Feat:ws 라우팅 변경

--- together_back/config/routing.py
+++ together_back/config/routing.py
@@ -2,8 +2,7 @@ from django.urls import path
 
 from chattings.consumers import ChattingConsumer
 
-# 127.0.0.1:8000/chattings/<room_name>
+# 127.0.0.1:8000/chattings/<conversation_name>
 websocket_urlpatterns = [
-    path("chattings", ChattingConsumer.as_asgi()),
-    # path(r"^chattings/(?P<room_name>[^/]+)/$", ChattingConsumer.as_asgi()),
+    path("chattings/<conversation_name>/", ChattingConsumer.as_asgi()),
 ]
