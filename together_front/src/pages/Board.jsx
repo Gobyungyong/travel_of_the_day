1254a3d5880a4188f488d9b748385e00b132129e
byungyong <quddyddlrk@naver.com>
2023-09-07 20:20:59 +0900

    Feat:채팅하기 버튼 생성

--- together_front/src/pages/Board.jsx
+++ together_front/src/pages/Board.jsx
@@ -1,31 +1,45 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
+import Loading from "../components/uiux/Loading";
 
 function Board() {
   const { authAxios } = useContext(AuthContext);
   const { boardId } = useParams();
   const [board, setBoard] = useState();
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
 
   async function getBoardDetail(board_id) {
     const res = await authAxios.get(`api/v1/boards/${boardId}/`);
     setBoard(res.data);
-    console.log(board);
   }
+
+  async function getUserInfo() {
+    const res = await authAxios.get("api/v1/users/myinfo/");
+    await setUser(res.data);
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
   useEffect(() => {
     getBoardDetail(boardId);
   }, [boardId]);
 
-  if (!board) {
-    return <div>loading...</div>;
+  if (!board || !user) {
+    return <Loading />;
   }
-
+  // 눈에 보이는 username 닉네임으로 변경
   return (
     <>
       <div>제목:{board.subject}</div>
-      <div>작성자:{board.writer.name}</div>
+      <div>작성자:{board.writer.username}</div>
       <div>내용:{board.content}</div>
+      <Link to={`/chattings/${board.writer.username}__${user?.username}`}>
+        <div>{board.writer.username}</div>
+      </Link>
     </>
   );
 }
