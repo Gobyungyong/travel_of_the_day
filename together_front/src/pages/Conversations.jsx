7d4798bf0bd2659d259a04c4c6be857df207e881
byungyong <quddyddlrk@naver.com>
2023-09-07 22:55:48 +0900

    Feat:date format 함수 수정

--- together_front/src/pages/Conversations.jsx
+++ together_front/src/pages/Conversations.jsx
@@ -10,7 +10,7 @@ function Conversations() {
   const [user, setUser] = useState();
 
   async function getConversationList() {
-    const res = await authAxios("api/v1/chattings/");
+    const res = await authAxios("api/v1/chattings/conversations/");
     setConversations(res.data);
     console.log("res", res);
   }
@@ -36,7 +36,19 @@ function Conversations() {
   function formatMessageTimestamp(timestamp) {
     if (!timestamp) return;
     const date = new Date(timestamp);
-    return date.toLocaleTimeString().slice(0, 5);
+    const formattedDate = {
+      year: date.getFullYear(),
+      month: date.getMonth(),
+      day: date.getDay(),
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+    };
+    console.log("date", formattedDate);
+    // return date.toLocaleTimeString().slice(0, 7);
+    return {
+      date: `${formattedDate.year}-${formattedDate.month}-${formattedDate.day}`,
+      hours: `${formattedDate.hour}:${formattedDate.minute}`,
+    };
   }
 
   if (!conversations || !user) {
@@ -47,13 +59,15 @@ function Conversations() {
     <div>
       {conversations.map((c) => (
         <Link
-          to={`/chattings/${createConversationName(c.other_user.username)}`}
-          key={c.other_user.username}
+          to={`/chattings/${createConversationName(c.other_user?.username)}`}
+          key={c.other_user?.username}
         >
-          {c.other_user.username}
-          <p className="text-gray-700">
-            {formatMessageTimestamp(c.last_message?.timestamp)}
-          </p>
+          {c.other_user?.username}
+          <div className="text-gray-700">
+            {formatMessageTimestamp(c.last_message?.timestamp).date}
+          </div>
+          <div>{formatMessageTimestamp(c.last_message?.timestamp).hours}</div>
+          <div>마지막 메세지:{c.last_message?.content}</div>
         </Link>
       ))}
     </div>
