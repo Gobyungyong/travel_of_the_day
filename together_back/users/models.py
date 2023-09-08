15b4676b6c075a5e8727afab005a4b48c2cb4bab
byungyong <quddyddlrk@naver.com>
2023-09-08 14:33:08 +0900

    Comment:닉네임,아바타 필드 작성

--- together_back/users/models.py
+++ together_back/users/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=10)
+    # nickname = models.CharField(max_length=10)
+    # avatar = models.URLField()
