f4614cc3d93de9f682786bf4e9bdf222c14b851d
byungyong <quddyddlrk@naver.com>
2023-09-10 19:34:04 +0900

    Feat:검색 기능 추가

--- together_front/src/pages/Together.jsx
+++ together_front/src/pages/Together.jsx
@@ -17,21 +17,39 @@ function Together() {
     getAllBoards();
   }, []);
 
+  async function SearchBoards(e) {
+    e.preventDefault();
+    const category = e.target.category.value;
+    const keyword = e.target.keyword.value;
+    const searchResults = await authAxios.get(
+      `api/v1/boards/search/?category=${category}&keyword=${keyword}`
+    );
+    console.log("이벤트타겟", e.target.category.value);
+    await setBoards(searchResults.data);
+  }
+
   if (!boards) {
-    return <div>loading...</div>;
+    return <div>게시글이 존재하지 않습니다.</div>;
   }
 
   return (
     <>
-      {boards.length > 0 ? (
-        boards.map((board) => (
-          <div key={board.id}>
-            <Link to={`/board/${board.id}`}>{board.subject}</Link>
-          </div>
-        ))
-      ) : (
-        <div>아직 게시글이 없습니다.</div>
-      )}
+      <form onSubmit={SearchBoards}>
+        <select name="category">
+          <option value={"all"}>전체</option>
+          <option value={"board"}>게시글</option>
+          <option value={"comment"}>댓글</option>
+          <option value={"boardcomment"}>게시글+댓글</option>
+          <option value={"writer"}>작성자</option>
+        </select>
+        <input type="text" name="keyword" placeholder="검색어를 입력하세요" />
+        <button>검색</button>
+      </form>
+      {boards.map((board) => (
+        <div key={board.id}>
+          <Link to={`/board/${board.id}`}>{board.subject}</Link>
+        </div>
+      ))}
     </>
   );
 }
