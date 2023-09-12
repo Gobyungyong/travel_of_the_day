855f3be1d3e03aa3cc300d7d4056b05acf9851c2
unknown <quddyddlrk@naver.com>
2023-09-13 02:32:34 +0900

    Design:게시글 댓글 수 작성

--- together_front/src/pages/Together.jsx
+++ together_front/src/pages/Together.jsx
@@ -119,7 +119,7 @@ function Together() {
                       {formatMessageTimestamp(board.created_at).hours}
                     </time>
                   </div>
-                  <div className="group relative">
+                  <div className="group relative w-full">
                     <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                       <Link to={`/board/${board.id}`}>
                         <span className="absolute inset-0" />
@@ -129,8 +129,25 @@ function Together() {
                     <p className="mt-5 px-2 line-clamp-3 text-sm leading-6 text-gray-600">
                       {board.content}
                     </p>
+                    <p className="mt-3 flex space-x-3 absolute right-0 ">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
+                        />
+                      </svg>
+                      <span> {board.comments_count}</span>
+                    </p>
                   </div>
-                  <div className="relative mt-8 flex items-center gap-x-4">
+                  <div className="relative mt-5 flex items-center gap-x-4">
                     <img
                       src={board.writer.avatar}
                       alt=""
