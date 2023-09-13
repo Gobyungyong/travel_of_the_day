eff5b81ab6d43dfa12882897bc25ba2b36033fa3
byungyong <quddyddlrk@naver.com>
2023-09-13 13:04:04 +0900

    Design:채팅목록 큰 화면 padding 추가

--- together_front/src/pages/Conversations.jsx
+++ together_front/src/pages/Conversations.jsx
@@ -101,7 +101,7 @@ function Conversations() {
   console.log(conversations);
   return (
     <>
-      <ul role="list" className="divide-y divide-gray-100 px-5">
+      <ul role="list" className="divide-y divide-gray-100 px-5 lg:px-36">
         {conversations.map((c) => (
           <li key={c.id}>
             <Link
