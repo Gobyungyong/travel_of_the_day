2d744efa607cbd4a3c368c2b82016e7523787769
byungyong <quddyddlrk@naver.com>
2023-09-13 21:11:12 +0900

    Chore:댓글/대댓글 삭제 url 수정

--- together_back/comments/urls.py
+++ together_back/comments/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 # api/v1/comments/
 urlpatterns = [
-    path("<int:board_id>/", views.AllComments.as_view()),
     path("new/", views.NewComment.as_view()),
-    path("<int:comment_id>/", views.DeleteComment.as_view()),
+    path("delete/<int:comment_id>/", views.DeleteComment.as_view()),
+    path("<int:board_id>/", views.AllComments.as_view()),
 ]
--- together_back/recomments/urls.py
+++ together_back/recomments/urls.py
@@ -5,5 +5,5 @@ from . import views
 # api/v1/recomments/
 urlpatterns = [
     path("new/", views.NewRecomment.as_view()),
-    path("<int:recomment_id>/", views.DeleteRecomment.as_view()),
+    path("delete/<int:recomment_id>/", views.DeleteRecomment.as_view()),
 ]
