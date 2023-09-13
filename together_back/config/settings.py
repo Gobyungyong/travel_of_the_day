58f0ff7ac00a2bacaa946a180707a928c8eea60d
byungyong <quddyddlrk@naver.com>
2023-09-14 05:59:35 +0900

    Chore:환경변수 설정

--- together_back/config/settings.py
+++ together_back/config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("False")
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
