f42a90d59c245f338bc4d0fb03d1272ec48d5575
byungyong <quddyddlrk@naver.com>
2023-09-13 16:47:38 +0900

    Design:게시글 작성 css 적용

--- together_front/src/pages/BoardEditer.jsx
+++ together_front/src/pages/BoardEditer.jsx
@@ -21,10 +21,33 @@ function BoardEditer() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onValid)}>
-      <input {...register("subject")} />
-      <textarea {...register("content")} />
-      <button>저장</button>
+    <form
+      onSubmit={handleSubmit(onValid)}
+      className="flex flex-col space-y-4 px-4 lg:px-48"
+    >
+      <div className="flex flex-col space-y-4">
+        <label className="font-semibold text-2xl text-gray-500">제목</label>
+        <input
+          placeholder=" 제목을 입력하세요."
+          {...register("subject")}
+          className="border border-indigo-400 p-2 rounded-md focus:outline-none focus:border-indigo-700 focus:border-2"
+        />
+      </div>
+      <div className="flex flex-col space-y-4">
+        <label className="font-semibold text-2xl text-gray-500">내용</label>
+        <textarea
+          {...register("content")}
+          maxLength={150}
+          rows={10}
+          placeholder="내용을 입력하세요."
+          className="border border-indigo-400 resize-none p-4 rounded-md focus:outline-none focus:border-indigo-700 focus:border-2"
+        />
+      </div>
+      <div className="flex flex-row-reverse md:px-2">
+        <button className="flex w-full lg:w-1/3 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          저장하기
+        </button>
+      </div>
     </form>
   );
 }
